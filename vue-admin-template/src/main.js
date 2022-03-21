bfbb6d700519406c6b985780675a869aa786db7e
MybeBin <11017294@qq.com>
2022-03-22 00:35:56 +0800

    笔记管理添加markdown依赖

--- vue-admin-template/src/main.js
+++ vue-admin-template/src/main.js
@@ -32,6 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+Vue.use(mavonEditor)
 
 Vue.config.productionTip = false
 
