1cb61f2a240deb1e552d62d05ad6cd5fb528f106
maybeBin <11017294@qq.com>
2022-09-29 17:17:26 +0800

    添加docker配置

--- vue_notes_web/vue.config.js
+++ vue_notes_web/vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+    publicPath: '/',
     devServer: {
         port: 8888,
         proxy: {
             '/notes': {
+                // target: 'http://192.168.32.128:9000', //这里填写项目真实的后台接口地址
                 target: 'http://127.0.0.1:9000', //这里填写项目真实的后台接口地址
                 changOrigin: true, //设置允许跨域
                 pathRewrite: {
