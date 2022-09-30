88f66081b0ccf39bb823feba122a5ef153686990
maybeBin <11017294@qq.com>
2022-09-30 16:32:03 +0800

    修改docker配置文件

--- vue_notes_web/vue.config.js
+++ vue_notes_web/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         proxy: {
             '/notes': {
                 // target: 'http://192.168.32.128:9000', //这里填写项目真实的后台接口地址
-                target: 'http://127.0.0.1:9000', //这里填写项目真实的后台接口地址
+                target: 'http://127.0.0.1:9090/api', //这里填写项目真实的后台接口地址
                 changOrigin: true, //设置允许跨域
                 pathRewrite: {
                     '^/notes': ''
